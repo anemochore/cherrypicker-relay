588c1bac3b44829b50573b6743b065c98a552f2b
anemochore <fallensky@naver.com>
2023-06-18 17:45:23 +0900

    test commit

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const SQL = initSqlJs({
 async function run(e) {
   //init
   console.log2('기다리라우...');
-  const output = document.getElementById('output');
+  const output = document.getElementById('content');
   output.style.backgroundImage = 'url("spinner.gif")';
   output.alt = 'loading...';
 
@@ -46,8 +46,8 @@ async function run(e) {
 
   //load it
   file = {id: '1AeLjyBb6pExwYcvVRsIRz8gC7l7FrqxZ'};  //dev
-  const url = getUrl(file.id);
-  //url = 'https://github.com/lerocha/chinook-database/raw/master/ChinookDatabase/DataSources/Chinook_Sqlite.sqlite';  //dev
+  //const url = getUrl(file.id);
+  url = 'https://github.com/lerocha/chinook-database/raw/master/ChinookDatabase/DataSources/Chinook_Sqlite.sqlite';  //dev
   const db = loadSql(url);
 
   //query it
@@ -89,8 +89,104 @@ async function loadSql(url) {
 
 
 //gapi boilerplates
-google.accounts.id.initialize({
-  client_id: '484499455751-m0ck74mc8lkiffj3t6o7p7sk08jerlsk.apps.googleusercontent.com',
-  callback: run
-});
-google.accounts.id.prompt();
+const CLIENT_ID = '484499455751-m0ck74mc8lkiffj3t6o7p7sk08jerlsk.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyAwh4ElKmAhfmTBmdimD9vPAuGws6chirg';
+const SCOPE = 'http://www.googleapis.com/auth/drive.readonly';
+const DISCOVERY_DOC = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+document.getElementById('authorize_button').style.visibility = 'hidden';
+document.getElementById('signout_button').style.visibility = 'hidden';
+
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+  gapi.load('client', initializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function initializeGapiClient() {
+  await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+  });
+  gapiInited = true;
+  maybeEnableButtons();
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+  /*
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: '', // defined later
+  });
+  */
+
+  //from google one log-in example
+  google.accounts.id.initialize({
+    client_id: CLIENT_ID,
+    callback: '',
+  });
+  google.accounts.id.prompt();
+  
+  gisInited = true;
+  maybeEnableButtons();
+}
+
+/**
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+  if (gapiInited && gisInited) {
+    document.getElementById('authorize_button').style.visibility = 'visible';
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw (resp);
+    }
+    document.getElementById('signout_button').style.visibility = 'visible';
+    document.getElementById('authorize_button').innerText = '다시 로그인';
+    await run();
+  };
+
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({prompt: 'consent'});
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({prompt: ''});
+  }
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick() {
+  const token = gapi.client.getToken();
+  if (token !== null) {
+    google.accounts.oauth2.revoke(token.access_token);
+    gapi.client.setToken('');
+    //document.getElementById('content').innerText = '';
+    document.getElementById('authorize_button').innerText = '구글 로그인';
+    document.getElementById('signout_button').setAttributestyle.visibility = 'hidden';
+  }
+}
